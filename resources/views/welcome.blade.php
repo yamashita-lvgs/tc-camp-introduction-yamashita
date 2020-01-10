92ef1313d10a0e3c60efe687c43fb5f46345d78b
山下裕香 <yuka.yamashita@PC-2298.local>
2020-01-10 17:28:14 +0900

    [mod]ファイル戻し

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,8 +86,7 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracast</a>
-                    <a href="https://laracasts.com">Laracas</a>
+                    <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
