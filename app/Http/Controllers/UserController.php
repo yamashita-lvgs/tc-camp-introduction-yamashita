b5b1f4bc871f306eb5eadf20f77595ef87c0306d
yamashita-lvgs <yuka.yamashita@leverages.jp>
2020-01-20 21:08:26 +0900

    [mod]一覧作った

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use illuminate\Http\Request;
+
 class UserController extends Controller
 {
     public function index()
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -5,6 +5,23 @@
 </head>
 <body>
 <h1>User/Index</h1>
-
+<h1>ユーザー一覧</h1>
+    <table>
+        <tr>
+	    <th>id</th>
+	    <th>name</th>
+	    <th>mail</th>
+        </tr>
+        <tr>  
+	    <td>1</td>
+	    <td>hanako</td>
+            <td>hanako@flower.jp</td>
+	</tr>
+        <tr>
+            <td>2</td>
+            <td>taro</td>
+            <td>taro@tarotaro.jp</td>
+        </tr>
+    </table>
 </body>
 </html>
