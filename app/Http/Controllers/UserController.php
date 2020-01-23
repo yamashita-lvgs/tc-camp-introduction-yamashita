0de7587483ef48ba22b8b61eb5d5aa783fc6f0b1
yuka_yamashita <yuka.yamashita@leverages.jp>
2020-01-23 15:27:37 +0900

    [mod]改行修正

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,7 +35,7 @@ class UserController extends Controller
         'email' => $request->email,
         'created_at' => now(),
         'updated_at' => now()
-    	]);
+    ]);
 	return redirect('/users');
     }
 }
