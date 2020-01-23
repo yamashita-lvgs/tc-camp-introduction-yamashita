85c41ce448393e3bbc8a40a8bd321c70214a33ae
yuka_yamashita <yuka.yamashita@leverages.jp>
2020-01-23 17:22:47 +0900

    [mod]改行修正とか

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,32 +5,34 @@ namespace App\Http\Controllers;
 use illuminate\Http\Request;
 use App\Models\User;
 
+/**
+ * ユーザー画面に関するクラス
+ */
 class UserController extends Controller
 {
-
-/**
- * ユーザー一覧画面
- */
+    /**
+     * ユーザー一覧画面
+     */
     public function index()
     {
         $users = User::all();
         return view('user.index', ['users' => $users]);
     }
 
-/**
- * ユーザー新規登録（情報入力）
- */
-    public function add(Request $request)
-    {
-        return view('user.add');
-    }
-
-/**
- * ユーザー新規登録（情報登録処理）
- */
+    /**
+     * ユーザー新規登録（情報入力）
+     */
     public function create(Request $request)
     {
-	    User::insert([
+        return view('user.create');
+    }
+
+    /**
+     * ユーザー新規登録（情報登録処理）
+     */
+    public function showCreateScreen(Request $request)
+    {
+        User::insert([
             'name' => $request->name,
             'email' => $request->email,
             'created_at' => now(),
