06c8dd49583778aefde9d4e9ddd49101c106fbf4
yamashita-lvgs <yuka.yamashita@leverages.jp>
2020-01-23 15:20:27 +0900

    [mod]レビュー修正2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,17 +7,26 @@ use App\Models\User;
 
 class UserController extends Controller
 {
+
+/**
+ * ユーザー一覧画面
+ */
     public function index()
     {
         $users = User::all();
         return view('user.index', ['users' => $users]);
     }
-
+/**
+ * ユーザー新規登録（情報入力）
+ */
     public function add(Request $request)
     {
         return view('user.add');
     } 
 
+/**
+ * ユーザー新規登録（情報登録処理）
+ */ 
     public function create(Request $request)
     {  
 	User::insert([
