eb7d5cdb498edc0567873cb78264faab892743e7
yamashita-lvgs <yuka.yamashita@leverages.jp>
2020-01-23 17:17:33 +0900

    [mod]名前変更

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,26 +8,26 @@ use App\Models\User;
 class UserController extends Controller
 {
 
-/**
- * ユーザー一覧画面
- */
+    /**
+     * ユーザー一覧画面
+     */
     public function index()
     {
         $users = User::all();
         return view('user.index', ['users' => $users]);
     }
 
-/**
- * ユーザー新規登録（情報入力）
- */
+    /**
+     * ユーザー新規登録（情報入力）
+     */
     public function create(Request $request)
     {
         return view('user.create');
     }
 
-/**
- * ユーザー新規登録（情報登録処理）
- */
+    /**
+     * ユーザー新規登録（情報登録処理）
+     */
     public function showCreateScreen(Request $request)
     {
 	    User::insert([
