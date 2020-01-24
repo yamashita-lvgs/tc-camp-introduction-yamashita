c99f908d15832d1f7b2fe59d133e27c6a970ce0b
yuka_yamashita <yuka.yamashita@leverages.jp>
2020-01-24 17:27:29 +0900

    [mod]インデント

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -7,24 +7,24 @@
     <body>
         <h1>ユーザー登録</h1>
         <table>
-             <form method="post">
-	     {{ csrf_field() }}
-	     <tr>
-                 <th>name: </th>
-                 <td><input type="text" name="name" value="{{old('name')}}"></td>
-	         @if ($errors->has('name'))
-                     <td>{{$errors->first('name')}}</td>
-                 @endif
-	     <tr>
-             </tr>
+            <form method="post">
+            {{ csrf_field() }}
+            <tr>
+                <th>name: </th>
+                <td><input type="text" name="name" value="{{old('name')}}"></td>
+                @if ($errors->has('name'))
+                    <td>{{$errors->first('name')}}</td>
+                @endif
+            <tr>
+            </tr>
                  <th>mail: </th>
                  <td><input type="text" name="email" value="{{old('email')}}"></td>
-	         @if ($errors->has('email'))
+	             @if ($errors->has('email'))
                      <td>{{$errors->first('email')}}</td>
-		 @endif
-             </tr>
-             <tr><th></th><td><input type="submit" value="send"></td></tr>
-             </form>
+                 @endif
+            </tr>
+            <tr><th></th><td><input type="submit" value="send"></td></tr>
+            </form>
         </table>
     </body>
 </html>
