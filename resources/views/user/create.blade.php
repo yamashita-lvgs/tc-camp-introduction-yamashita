c3bb5163c2b51b99f13c619e1e6eb545c9113a64
yuka_yamashita <yuka.yamashita@leverages.jp>
2020-01-27 11:09:28 +0900

    [mod]インデント細かい修正

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -8,22 +8,22 @@
         <h1>ユーザー登録</h1>
         <table>
             <form method="post">
-            {{ csrf_field() }}
-            <tr>
-                <th>name: </th>
-                <td><input type="text" name="name" value="{{old('name')}}"></td>
-                @if ($errors->has('name'))
-                    <td>{{$errors->first('name')}}</td>
-                @endif
-            <tr>
-            </tr>
-                 <th>mail: </th>
-                 <td><input type="text" name="email" value="{{old('email')}}"></td>
-                 @if ($errors->has('email'))
-                     <td>{{$errors->first('email')}}</td>
-                 @endif
-            </tr>
-            <tr><th></th><td><input type="submit" value="send"></td></tr>
+                {{ csrf_field() }}
+                <tr>
+                    <th>name: </th>
+                    <td><input type="text" name="name" value="{{old('name')}}"></td>
+                    @if ($errors->has('name'))
+                        <td>{{$errors->first('name')}}</td>
+                    @endif
+                <tr>
+                </tr>
+                    <th>mail: </th>
+                    <td><input type="text" name="email" value="{{old('email')}}"></td>
+                    @if ($errors->has('email'))
+                        <td>{{$errors->first('email')}}</td>
+                    @endif
+                </tr>
+                <tr><th></th><td><input type="submit" value="send"></td></tr>
             </form>
         </table>
     </body>
